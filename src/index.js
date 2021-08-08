6901a224143961f72dd2eef056ac5d474cde095e
wilsom171 <53121506+wilsom171@users.noreply.github.com>
2021-08-08 12:31:20 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 const app = require('./app');
 
 async function main() {
-    await app.listen(5000, '0.0.0.0');
+    await app.listen(process.env.PORT || 5000)
+
+    //await app.listen(5000, '0.0.0.0');
     console.log('localhost:', 5000);
 }
 
