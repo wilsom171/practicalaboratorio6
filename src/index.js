c715eb2cef4db1e56d8d4f4a1ce0c38e381bf794
wilsom171 <53121506+wilsom171@users.noreply.github.com>
2021-08-08 12:24:43 -0500

    domingo

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 const app = require('./app');
 
 async function main() {
-    await app.listen(5000);
-    console.log('Server on port', 5000);
+    await app.listen(5000, '0.0.0.0');
+    console.log('localhost:', 5000);
 }
 
 main();
